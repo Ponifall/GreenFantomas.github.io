0964fc3abaf4ceebc7fe5bafe58b34568203d84d
Egor Sheff <egor.sheff@gmail.com>
2021-01-21 16:29:20 +0300

    add diff

--- wolfram.js
+++ wolfram.js
@@ -12,3 +12,24 @@ function maxRedirect() {
   func = $("#maxFunc").val();
   window.open(wolframURL + "max+z=" + encodeURIComponent(func));
 }
+
+function diffRedirect() {
+  func = $("#diffFunc").val();
+  x = $("#diffX").val();
+  if (func == "") {
+    $("#diffToast").toast("show");
+    return;
+  }
+
+  if (x == "") {
+    window.open(wolframURL + "y=(" + encodeURIComponent(func) + ")'");
+  } else {
+    window.open(
+      wolframURL +
+        "y=(" +
+        encodeURIComponent(func) +
+        ")',+x=" +
+        encodeURIComponent(x)
+    );
+  }
+}
